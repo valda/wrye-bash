9a1f232f4483f99778bcf4376b7b45226a88a21d
Sharlikran <danielhmpdx16@gmail.com>
2014-08-28 07:38:05 -0700

    These values don't belong to AIDT

--- Mopy/bash/game/falloutnv.py
+++ Mopy/bash/game/falloutnv.py
@@ -1757,24 +1757,7 @@ class MreCrea(MreActor):
         (16,'recharge'),
         (17,'repair'),))
 
-        # trainSkill
-        # -1, None
-        #  0, Barter
-        #  1, Big Guns (obsolete)
-        #  2, Energy Weapons
-        #  3, Explosives
-        #  4, Lockpick
-        #  5, Medicine
-        #  6, Melee Weapons
-        #  7, Repair
-        #  8, Science
-        #  9, Guns
-        # 10, Sneak
-        # 11, Speech
-        # 12, Survival
-        # 13, Unarmed
-
-        #--Mel Set
+    #--Mel Set
     melSet = MelSet(
         MelString('EDID','eid'),
         MelStruct('OBND','=6h',

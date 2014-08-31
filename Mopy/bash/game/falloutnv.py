d5389455548b1b381b38f23bdbacda615d7e02c0
Sharlikran <danielhmpdx16@gmail.com>
2014-08-30 18:44:15 -0700

    Added MrePgre

--- Mopy/bash/game/falloutnv.py
+++ Mopy/bash/game/falloutnv.py
@@ -4475,6 +4475,57 @@ class MrePerk(MelRecord):
     __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
 
 #------------------------------------------------------------------------------
+class MrePgre(MelRecord):
+    """Placed Grenade"""
+    classType = 'PGRE'
+    _flags = Flags(0L,Flags.getNames('oppositeParent'))
+    _variableFlags = Flags(0L,Flags.getNames('isLongOrShort'))
+    _watertypeFlags = Flags(0L,Flags.getNames('reflection','refraction'))
+    melSet=MelSet(
+        MelString('EDID','eid'),
+        MelFid('NAME','base'),
+        MelFid('XEZN','encounterZone'),
+        MelBase('XRGD','ragdollData'),
+        MelBase('XRGB','ragdollBipedData'),
+        MelGroup('patrolData',
+            MelStruct('XPRD','f','idleTime'),
+            MelBase('XPPA','patrolScriptMarker'),
+            MelFid('INAM', 'idle'),
+            MelStruct('SCHR','4s4I',('unused1',null4),'numRefs','compiledSize','lastIndex','scriptType'),
+            MelBase('SCDA','compiled_p'),
+            MelString('SCTX','scriptText'),
+            MelGroups('vars',
+                MelStruct('SLSD','I12sB7s','index',('unused1',null4+null4+null4),(_variableFlags,'flags',0L),('unused2',null4+null3)),
+                MelString('SCVR','name')),
+            MelScrxen('SCRV/SCRO','references'),
+            MelFid('TNAM','topic'),
+            ),
+        MelOwnership(),
+        MelStruct('XCNT','i','count'),
+        MelStruct('XRDS','f','radius',),
+        MelStruct('XHLP','f','health',),
+        MelGroups('reflectedRefractedBy',
+            MelStruct('XPWR','2I',(FID,'waterReference'),(_watertypeFlags,'waterFlags',0L),),
+        ),
+        MelStructs('XDCR','II','linkedDecals',(FID,'reference'),'unknown'),
+        MelFid('XLKR','linkedReference'),
+        MelOptStruct('XCLP','8B','linkStartColorRed','linkStartColorGreen','linkStartColorBlue',('linkColorUnused1',null1),
+                     'linkEndColorRed','linkEndColorGreen','linkEndColorBlue',('linkColorUnused2',null1)),
+        MelGroup('activateParents',
+            MelStruct('XAPD','B','flags'),
+            MelStructs('XAPR','If','activateParentRefs',(FID,'reference'),'delay')
+            ),
+        MelString('XATO','activationPrompt'),
+        MelOptStruct('XESP','IB3s',(FID,'parent'),(_flags,'parentFlags'),('unused1',null3)),
+        MelOptStruct('XEMI','I',(FID,'emitance')),
+        MelFid('XMBR','multiboundReference'),
+        MelBase('XIBS','ignoredBySandbox'),
+        MelOptStruct('XSCL','f',('scale',1.0)),
+        MelOptStruct('DATA','=6f',('posX',None),('posY',None),('posZ',None),('rotX',None),('rotY',None),('rotZ',None)),
+    )
+    __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
+
+#------------------------------------------------------------------------------
 class MreProj(MelRecord):
     """Projectile record."""
     classType = 'PROJ'
@@ -5948,7 +5999,7 @@ class MreWthr(MelRecord):
 # MreTerm, MreTree, MreTxst, MreVtyp, MreWatr, MreWeap, MreWthr,
 
 mergeClasses = (
-        # MreAchr, MreAcre, MreGmst,
+        # MreAchr, MreAcre, MreGmst, MrePgre,
         MreActi, MreAddn, MreAlch, MreAloc, MreAmef, MreAmmo, MreAnio, MreArma, MreArmo, MreAspc,
         MreAvif, MreBook, MreBptd, MreCams, MreCcrd, MreCdck, MreChal, MreChip, MreClas, MreClmt,
         MreCmny, MreCobj, MreCont, MreCpth, MreCrea, MreCsno, MreCsty, MreDebr, MreDehy, MreDobj, 
@@ -5990,13 +6041,13 @@ def init():
     brec.ModReader.recHeader = RecordHeader
 
     # Need to add
-    # 'RGDL', 'PGRE',
+    # 'RGDL',
 
     #--Record Types
     brec.MreRecord.type_class = dict((x.classType,x) for x in (
 
         # Verified
-        MreAchr, MreAcre, MreGmst,
+        MreAchr, MreAcre, MreGmst, MrePgre,
         MreActi, MreAddn, MreAlch, MreAloc, MreAmef, MreAmmo, MreAnio, MreArma, MreArmo, MreAspc,
         MreAvif, MreBook, MreBptd, MreCams, MreCcrd, MreCdck, MreChal, MreChip, MreClas, MreClmt,
         MreCmny, MreCobj, MreCont, MreCpth, MreCrea, MreCsno, MreCsty, MreDebr, MreDehy, MreDobj, 

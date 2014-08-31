296dd71c1c932c5e0c47020c1ac94ce5b5b51fbb
valda <valda@underscore.jp>
2014-08-31 13:17:31 +0900

    Fixed NamesPatcher docstring.

--- Mopy/bash/bosh.py
+++ Mopy/bash/bosh.py
@@ -14555,7 +14555,7 @@ class CBash_ImportActorsSpells(CBash_ImportPatcher):
 from patcher.oblivion.utilities import FullNames, CBash_FullNames
 
 class NamesPatcher(ImportPatcher):
-    """Merged leveled lists mod file."""
+    """Import names from source mods/files."""
     name = _(u'Import Names')
     text = _(u"Import names from source mods/files.")
     canAutoItemCheck = True #--GUI: Whether new items are checked by default or not.

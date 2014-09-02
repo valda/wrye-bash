9dfc5b9042263484071451d83fa14c3c2ca99c69
Sharlikran <danielhmpdx16@gmail.com>
2014-09-02 12:44:08 -0700

    Updated MreProj

--- Mopy/bash/game/fallout3.py
+++ Mopy/bash/game/fallout3.py
@@ -4126,16 +4126,18 @@ class MrePgre(MelRecord):
 class MreProj(MelRecord):
     """Projectile record."""
     classType = 'PROJ'
-    _flags = Flags(0,Flags.getNames('hitscan',
-                                    'explosive',
-                                    'altTriger',
-                                    'muzzleFlash',
-                                    'unknown4',
-                                    'canbeDisable',
-                                    'canbePickedUp',
-                                    'superSonic',
-                                    'pinsLimbs',
-                                    'passThroughSmallTransparent'))
+    _flags = Flags(0,Flags.getNames(
+        'hitscan',
+        'explosive',
+        'altTriger',
+        'muzzleFlash',
+        None,
+        'canbeDisable',
+        'canbePickedUp',
+        'superSonic',
+        'pinsLimbs',
+        'passThroughSmallTransparent'
+        ))
     melSet = MelSet(
         MelString('EDID','eid'),
         MelStruct('OBND','=6h',
@@ -4150,9 +4152,10 @@ class MreProj(MelRecord):
                   ('explosionAltTrigerProximity',0.00000),('explosionAltTrigerTimer',0.00000),
                   (FID,'explosion',0),(FID,'sound',0),('muzzleFlashDuration',0.00000),
                   ('fadeDuration',0.00000),('impactForce',0.00000),
-                  (FID,'soundCountDown',0),(FID,'soundDisable',0),(FID,'defaultWeaponSource',0)),
+                  (FID,'soundCountDown',0),(FID,'soundDisable',0),
+                  (FID,'defaultWeaponSource',0),),
         MelString('NAM1','muzzleFlashPath'),
-        MelBase('NAM2','_nam2'), #--Should be a struct. Maybe later.
+        MelBase('NAM2','_nam2'),
         MelStruct('VNAM','I','soundLevel'),
         )
     __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()

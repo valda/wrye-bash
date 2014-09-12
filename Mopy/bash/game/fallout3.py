fb891a15246f34e297a607037953380a581dcbca
Sharlikran <danielhmpdx16@gmail.com>
2014-09-12 10:58:00 -0700

    Updated MreCont

--- Mopy/bash/game/fallout3.py
+++ Mopy/bash/game/fallout3.py
@@ -2147,12 +2147,12 @@ class MreCobj(MelRecord):
 class MreCont(MelRecord):
     """Container record."""
     classType = 'CONT'
-    _flags = bolt.Flags(0,bolt.Flags.getNames(None,'respawns'))
+    _flags = Flags(0,Flags.getNames(None,'respawns'))
     melSet = MelSet(
         MelString('EDID','eid'),
         MelStruct('OBND','=6h',
-                  'corner0X','corner0Y','corner0Z',
-                  'corner1X','corner1Y','corner1Z'),
+                  'boundX1','boundY1','boundZ1',
+                  'boundX2','boundY2','boundZ2'),
         MelString('FULL','full'),
         MelModel(),
         MelFid('SCRI','script'),

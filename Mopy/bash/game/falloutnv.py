07e0d1b6524db79eb19f2eeaa3e0a4c86722e04d
Sharlikran <danielhmpdx16@gmail.com>
2014-09-12 13:40:57 -0700

    FNV Updated MreRegn

--- Mopy/bash/game/falloutnv.py
+++ Mopy/bash/game/falloutnv.py
@@ -5239,12 +5239,6 @@ class MreRefr(MelRecord):
 class MreRegn(MelRecord):
     """Region record."""
     classType = 'REGN'
-    _flags = Flags(0L,Flags.getNames(
-        ( 2,'objects'),
-        ( 3,'weather'),
-        ( 4,'map'),
-        ( 6,'grass'),
-        ( 7,'sound'),))
     obflags = Flags(0L,Flags.getNames(
         ( 0,'conform'),
         ( 1,'paintVertices'),
@@ -5259,6 +5253,8 @@ class MreRegn(MelRecord):
         ( 1,'cloudy'),
         ( 2,'rainy'),
         ( 3,'snowy'),))
+    rdatFlags = Flags(0L,Flags.getNames(
+        ( 0,'Override'),))
 
     ####Lazy hacks to correctly read/write regn data
     class MelRegnStructA(MelStructA):
@@ -5312,7 +5308,7 @@ class MreRegn(MelRecord):
 
     melSet = MelSet(
         MelString('EDID','eid'),
-        MelString('ICON','largeIconPath'),
+        MelString('ICON','iconPath'),
         MelString('MICO','smallIconPath'),
         MelStruct('RCLR','3Bs','mapRed','mapBlue','mapGreen',('unused1',null1)),
         MelFid('WNAM','worldspace'),
@@ -5320,13 +5316,18 @@ class MreRegn(MelRecord):
             MelStruct('RPLI','I','edgeFalloff'),
             MelStructA('RPLD','2f','points','posX','posY')),
         MelGroups('entries',
-            MelStruct('RDAT', 'I2B2s','entryType', (_flags,'flags'), 'priority', ('unused1',null2)), ####flags actually an enum...
-            MelRegnStructA('RDOT', 'IH2sf4B2H4s4f3H2s4s', 'objects', (FID,'objectId'), 'parentIndex',
-            ('unused1',null2), 'density', 'clustering', 'minSlope', 'maxSlope',
-            (obflags, 'flags'), 'radiusWRTParent', 'radius', ('unk1',null4),
-            'maxHeight', 'sink', 'sinkVar', 'sizeVar', 'angleVarX',
-            'angleVarY',  'angleVarZ', ('unused2',null2), ('unk2',null4)),
+            #2:Objects,3:Weather,4:Map,5:Land,6:Grass,7:Sound
+            MelStruct('RDAT', 'I2B2s','entryType', (rdatFlags,'flags'), 'priority',
+                     ('unused1',null2)),
+            MelRegnStructA('RDOT', 'IH2sf4B2H4s4f3H2s4s', 'objects', (FID,'objectId'),
+                           'parentIndex',('unused1',null2), 'density', 'clustering',
+                           'minSlope', 'maxSlope',(obflags, 'flags'), 'radiusWRTParent',
+                           'radius', ('unk1',null4),'maxHeight', 'sink', 'sinkVar',
+                           'sizeVar', 'angleVarX','angleVarY',  'angleVarZ',
+                           ('unused2',null2), ('unk2',null4)),
             MelRegnString('RDMP', 'mapName'),
+            MelRegnStructA('RDGS', 'I4s', 'grass', ('unknown',null4)),
+            MelRegnOptStruct('RDMD', 'I', 'musicType'),
             MelFid('RDMO','music'),
             MelFid('RDSI','incidentalMediaSet'),
             MelFids('RDSB','battleMediaSets'),

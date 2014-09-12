20426fd4958bfe37441eaf552a922722705249c4
Sharlikran <danielhmpdx16@gmail.com>
2014-09-12 10:49:51 -0700

    Updated MreLeveledList

--- Mopy/bash/game/fallout3.py
+++ Mopy/bash/game/fallout3.py
@@ -1302,8 +1302,8 @@ class MreLeveledList(MelRecord):
     melSet = MelSet(
         MelString('EDID','eid'),
         MelStruct('OBND','=6h',
-                  'corner0X','corner0Y','corner0Z',
-                  'corner1X','corner1Y','corner1Z'),
+                  'boundX1','boundY1','boundZ1',
+                  'boundX2','boundY2','boundZ2'),
         MelLevListLvld('LVLD','B','chanceNone'),
         MelStruct('LVLF','B',(_flags,'flags',0L)),
         MelFid('SCRI','script'),

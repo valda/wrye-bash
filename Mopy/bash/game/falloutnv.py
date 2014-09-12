00529bf76680216a4f1b870e3eb5cf866ac76c7e
Sharlikran <danielhmpdx16@gmail.com>
2014-09-12 13:44:33 -0700

    FNV Updated MreTact

--- Mopy/bash/game/falloutnv.py
+++ Mopy/bash/game/falloutnv.py
@@ -5622,8 +5622,8 @@ class MreTact(MelRecord):
     melSet = MelSet(
         MelString('EDID','eid'),
         MelStruct('OBND','=6h',
-                  'corner0X','corner0Y','corner0Z',
-                  'corner1X','corner1Y','corner1Z'),
+                  'boundX1','boundY1','boundZ1',
+                  'boundX2','boundY2','boundZ2'),
         MelString('FULL','full'),
         MelModel('model'),
         MelFid('SCRI','script'),

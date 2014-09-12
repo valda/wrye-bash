9049132045e9557438640a8f9187317b09d5d93a
Sharlikran <danielhmpdx16@gmail.com>
2014-09-12 11:19:40 -0700

    Updated MreStat

--- Mopy/bash/game/fallout3.py
+++ Mopy/bash/game/fallout3.py
@@ -5038,8 +5038,8 @@ class MreStat(MelRecord):
     melSet = MelSet(
         MelString('EDID','eid'),
         MelStruct('OBND','=6h',
-                  'corner0X','corner0Y','corner0Z',
-                  'corner1X','corner1Y','corner1Z'),
+                  'boundX1','boundY1','boundZ1',
+                  'boundX2','boundY2','boundZ2'),
         MelModel(),
         )
     __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()

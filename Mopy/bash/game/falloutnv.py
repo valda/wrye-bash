27061f0991c297cc55c643196fea67646eff2bb9
Sharlikran <danielhmpdx16@gmail.com>
2014-09-13 20:25:13 -0700

    Disabled Graphics Patcher until I can add Game Specific Attrs

--- Mopy/bash/game/falloutnv.py
+++ Mopy/bash/game/falloutnv.py
@@ -699,14 +699,14 @@ GmstTweaks = [
 allTags = sorted((
     u'C.Climate', u'C.Light', u'C.Music', u'C.Name', u'C.Owner', u'C.RecordFlags',
     u'C.Water', u'Deactivate', u'Deflst', u'Delev', u'Destructible', u'Factions',
-    u'Filter', u'Graphics', u'Names', u'NoMerge', u'Relations', u'Relev', u'Sound',
+    u'Filter', u'Names', u'NoMerge', u'Relations', u'Relev', u'Sound',
     u'Stats', u'WeaponMods',
     ))
 
 #--Patcher available when building a Bashed Patch (referenced by class name)
 patchers = (
     u'AliasesPatcher', u'CellImporter', u'DestructiblePatcher', u'FidListsMerger',
-    u'GmstTweaker', u'GraphicsPatcher', u'ImportFactions', u'ImportRelations',
+    u'GmstTweaker', u'ImportFactions', u'ImportRelations',
     u'ListsMerger', u'NamesPatcher', u'PatchMerger', 'SoundPatcher', u'StatsPatcher',
     u'WeaponModsPatcher',
     )
@@ -714,17 +714,21 @@ patchers = (
 #--CBash patchers available when building a Bashed Patch
 CBash_patchers = (
 )
-
-#--For ListMerger patcher (leveled list patcher)
+#-------------------------------------------------------------------------------
+# ListsMerger
+#-------------------------------------------------------------------------------
 listTypes = ('LVLC','LVLI','LVLN')
-# Needs longs in SoundPatcher
-soundsLongsTypes = set(('ACTI','ADDN','ALCH','ASPC','CONT','DOOR','LIGH','MGEF','WTHR','WEAP'))
-
+#-------------------------------------------------------------------------------
+# NamesPatcher
+#-------------------------------------------------------------------------------
 namesTypes = set((
     'ACTI','ALCH','AMMO','ARMO','BOOK','CCRD','CHIP','CLAS','CMNY','CONT','CREA',
     'CSNO','DOOR','EYES','FACT','HAIR','IMOD','INGR','KEYM','LIGH','MISC','NOTE',
     'NPC_','RACE','RCCT','RCPE','REPU','SPEL','TACT','TERM','WEAP',
     ))
+#-------------------------------------------------------------------------------
+# ItemPrices Patcher
+#-------------------------------------------------------------------------------
 pricesTypes = {'ALCH':{},'AMMO':{},'ARMO':{},'ARMA':{},'BOOK':{},'INGR':{},'KEYM':{},'LIGH':{},'MISC':{},'WEAP':{}}
 
 #-------------------------------------------------------------------------------
